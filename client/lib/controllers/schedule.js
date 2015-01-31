ec9881eb2618d32b606f86c2efbb60f157b44fd1
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2015-01-31 18:30:56 -0200

    Improve schedule and agenda subs

--- client/lib/controllers/schedule.js
+++ client/lib/controllers/schedule.js
@@ -1,19 +1,2 @@
-ScheduleController = RouteController.extend({
-  waitOn: function () {
-    return [
-      Meteor.subscribe('schedule')
-    ];
-  },
-
-  data: function () {
-  },
-
-  onBeforeAction: function () {
-    if (! Meteor.user()) {
-      this.redirect(Router.routes.login.path());
-      Session.set('nextPage', this.path);
-      this.stop();
-      return;
-    }
-  }
+ScheduleController = LoggedUserController.extend({
 });
--- server/publish/schedule.js
+++ server/publish/schedule.js
@@ -1,4 +1,4 @@
-Meteor.publish('schedule', function (_id, activeOnly) {
+Meteor.publish('Schedule', function (_id, activeOnly) {
   var query = {};
 
   if (_id) {
@@ -15,4 +15,43 @@ Meteor.publish('schedule', function (_id, activeOnly) {
   }
 
   return Schedule.find(query);
+});
+
+Meteor.publishComposite('OneScheduleWithProfile', function (_id) {
+  return {
+    find: function () {
+      return Schedule.find({_id: _id});
+    },
+    children: [{
+      find: function (schedule) {
+        return Meteor.users.find({_id: schedule.owner}, {
+          profile: 1,
+          superchat: 1
+        });
+      }
+    }]
+  };
+});
+
+Meteor.publishComposite('EntireAgendaWithProfiles', function () {
+  return {
+    find: function () {
+      return Schedule.find({
+        $or: [{
+          owner: this.userId,
+          isActive: false
+        }, {
+          isActive: true
+        }]
+      });
+    },
+    children: [{
+      find: function (schedule) {
+        return Meteor.users.find({_id: schedule.owner}, {
+          profile: 1,
+          superchat: 1
+        });
+      }
+    }]
+  };
 });
