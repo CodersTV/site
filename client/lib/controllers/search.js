a338fe923d7bfa6ab44d24245e872d9cbd4b28cc
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2015-03-29 12:38:32 -0300

    Fix search not showing proper results

--- client/lib/controllers/search.js
+++ client/lib/controllers/search.js
@@ -1,4 +1,4 @@
-SearchController = CoderListController.extend({
+SearchController = RouteController.extend({
   template: 'channels',
   waitOn: function () {
     return Meteor.subscribe('ChannelsSearchWithUsers', this.params.keyword);
