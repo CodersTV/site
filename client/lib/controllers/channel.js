658418375f60821ec2d3232b9dd7abe588d13830
Gabriel Pugliese <gabrielhpugliese@gmail.com>
2015-04-26 14:17:49 -0300

    Add SEO package and meta and og tags

--- client/lib/controllers/channel.js
+++ client/lib/controllers/channel.js
@@ -9,6 +9,8 @@ CoderController = RouteController.extend({
   },
   onAfterAction: function () {
     var coder = Meteor.users.findOneFromCoderId(this.params.coderId);
+    var channel = Channels.findOne();
+
     if (! _.isEmpty(coder)) {
       Session.set('coder', coder);
       Session.set('chatHost', coder._id);
@@ -21,6 +23,23 @@ CoderController = RouteController.extend({
           Session.set('chatSubsReady', true);
         }
       );
+
+      SEO.set({
+        title: channel.title + ' by ' + coder.profile.name + ' | CodersTV',
+        rel_author: coder.superchat.url || '',
+        meta: {
+          description: channel.description
+        },
+        og: {
+          type: 'profile',
+          'profile:first_name': coder.superchat.givenName,
+          'profile:last_name': coder.superchat.familyName,
+          'profile:username': coder.profile.username || coder.profile.name,
+          title: channel.title,
+          description: channel.description,
+          image: coder.image
+        }
+      });
     }
   }
 });
@@ -36,9 +55,28 @@ VideoController = RouteController.extend({
   },
   onAfterAction: function () {
     var coder = Meteor.users.findOneFromCoderId(this.params.coderId);
+    var channel = Channels.findOne();
+
     if (! _.isEmpty(coder)) {
       Session.set('coder', coder);
       Session.set('currentCoder', coder._id);
+
+      SEO.set({
+        title: channel.title + ' by ' + coder.profile.name + ' | CodersTV',
+        rel_author: coder.superchat.url || '',
+        meta: {
+          description: channel.description
+        },
+        og: {
+          type: 'video.other',
+          'video:writer': coder.profile.name,
+          'video:release_date': (new Date(channel.finishedAt)).toISOString(),
+          'video:tags': channel.language,
+          title: channel.title,
+          description: channel.description,
+          image: coder.image
+        }
+      });
     }
   }
 });
